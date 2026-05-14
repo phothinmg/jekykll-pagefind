4b359b4b71bc2b2e616f203bdd3af94abf27a808
phothinmg <phothinmg@disroot.org>
2026-05-15 05:03:56 +0630

    new file:   .gitignore

---
+++ lib/jekyll/pagefind.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pagefind/version"
+
+module Jekyll
+  module Pagefind
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
